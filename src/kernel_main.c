d13d5ace617da6843b1c5909a5d96ecf1b8d3206
CrisArroyo <167390425+CrisTakashi@users.noreply.github.com>
2025-10-20 23:55:46 -0500

    Update kernel_main.c

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,9 +1,11 @@
-
 #include <stdint.h>
+#include "page.h"  // Added for page frame allocator
 
 #define MULTIBOOT2_HEADER_MAGIC         0xe85250d6
 
-const unsigned int multiboot_header[]  __attribute__((section(".multiboot"))) = {MULTIBOOT2_HEADER_MAGIC, 0, 16, -(16+MULTIBOOT2_HEADER_MAGIC), 0, 12};
+const unsigned int multiboot_header[]  __attribute__((section(".multiboot"))) = {
+    MULTIBOOT2_HEADER_MAGIC, 0, 16, -(16+MULTIBOOT2_HEADER_MAGIC), 0, 12
+};
 
 uint8_t inb (uint16_t _port) {
     uint8_t rv;
@@ -12,6 +14,16 @@ uint8_t inb (uint16_t _port) {
 }
 
 void main() {
+    init_pfa_list();
+
+    struct ppage *alloc1 = allocate_physical_pages(3);
+
+    struct ppage *alloc2 = allocate_physical_pages(2);
+
+    free_physical_pages(alloc1);
+
+    struct ppage *alloc3 = allocate_physical_pages(1);
+
     unsigned short *vram = (unsigned short*)0xb8000; // Base address of video mem
     const unsigned char color = 7; // gray text on black background
 
