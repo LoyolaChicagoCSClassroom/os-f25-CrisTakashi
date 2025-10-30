f2b8a3b0a6da0e6224ea528f921c51f3bf6640b5
CrisArroyo <167390425+CrisTakashi@users.noreply.github.com>
2025-10-29 21:48:42 -0500

    Update page.h HW 4

--- src/page.h
+++ src/page.h
@@ -1,6 +1,8 @@
 #ifndef PAGE_H
 #define PAGE_H
 
+#include <stdint.h>
+
 struct ppage {
     struct ppage *next;
     struct ppage *prev;
@@ -13,4 +15,32 @@ struct ppage *allocate_physical_pages(unsigned int npages);
 
 void free_physical_pages(struct ppage *ppage_list);
 
-#endif // PAGE_H
+struct page_directory_entry {
+    uint32_t present       : 1;
+    uint32_t rw            : 1;
+    uint32_t user          : 1;
+    uint32_t writethru     : 1;
+    uint32_t cachedisabled : 1;
+    uint32_t accessed      : 1;
+    uint32_t pagesize      : 1;
+    uint32_t ignored       : 2;
+    uint32_t os_specific   : 3;
+    uint32_t frame         : 20;
+};
+
+struct page {
+    uint32_t present    : 1;
+    uint32_t rw         : 1;
+    uint32_t user       : 1;
+    uint32_t accessed   : 1;
+    uint32_t dirty      : 1;
+    uint32_t unused     : 7;
+    uint32_t frame      : 20;
+};
+
+
+extern struct page_directory_entry pd[1024];
+extern struct page pt[1024];
+
+void *map_pages(void *vaddr, struct ppage *pglist, struct page_directory_entry *pd);
+#endif
